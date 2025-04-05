4eebd25d96c9a4598d61fe1325674ee615807c28
ridhampansuriya2000 <ridhampansuriya2000@gmail.com>
2025-04-05 17:24:36 +0530

    add domain in config

--- imdb/next.config.mjs
+++ imdb/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["image.tmdb.org", "via.placeholder.com", "example.com"],
+        domains: ["image.tmdb.org", "via.placeholder.com", "example.com", "https://demo-four-junctions.vercel.app", "https://demo-imdb.vercel.app/"],
         remotePatterns: [
             {
                 protocol: "https",
