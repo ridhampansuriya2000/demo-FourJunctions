66e7c6f7d599e8030b837d528328e9b3738864e7
ridhampansuriya2000 <ridhampansuriya2000@gmail.com>
2025-04-06 08:31:58 +0530

    Refector code

--- imdb-be/server.js
+++ imdb-be/server.js
@@ -4,17 +4,13 @@ const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const { connectDB } = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
-
-/** Import routes */
 const authRoutes = require('./routes/authRoutes');
 const movieRoutes = require('./routes/movieRoutes');
 const actorRoutes = require('./routes/actorRoutes');
 const producerRoutes = require('./routes/producerRoutes');
 
-/** Load environment variables */
 dotenv.config();
 
-/** Connect to MongoDB */
 connectDB();
 
 const app = express();
@@ -27,20 +23,13 @@ app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true
 }));
+app.use(errorHandler);
 
 /** Routes */
 app.use('/api/auth', authRoutes);
 app.use('/api/movies', movieRoutes);
 app.use('/api/actors', actorRoutes);
 app.use('/api/producers', producerRoutes);
-
-/** Health check route */
-app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'ok', message: 'Server is running' });
-});
-
-/** Error handling middleware */
-app.use(errorHandler);
 
 /** Start server */
 app.listen(PORT, () => {
